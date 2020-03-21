6501f9559fc7e1249350fa65ded1c6ca6e2e9f86
anna bateman <annacbateman@gmail.com>
2020-03-21 18:36:12 +0000

    Done.

---
+++ if_end.rb
@@ -0,0 +1,34 @@
+#//run_code_inside = false
+#puts "Code before if ...end"
+#if run_code_inside
+#puts "code inside"
+#end
+#puts "Code after if ...end"
+
+
+#chances_of_rain = 0.2
+#puts "Let's go outside!"
+#if chances_of_rain > 0.5
+  #puts "Pack an umbrella"
+#else 
+  #puts "Enjoy the fine day!"
+#end
+#puts "Oh, and always wear sunscrean!"
+
+
+#chance_of_rain = -23
+#if chance_of_rain <= 0.25
+  #puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#puts "Pack an umbrella!"
+#else 
+  #puts "Stay home and read Hegel"
+#end
+
+this_year = Time.now.year 
+puts "Hey, it's 2020!" unless this_year == 2019
+
+
+
+
+
