8343bc706bd89d258f722625a20270bb2060730a
anna bateman <annacbateman@gmail.com>
2020-03-22 20:00:35 +0000

    Done.

---
+++ learn_methods.rb
@@ -0,0 +1,19 @@
+#message = "Hello World!"
+#5.times do
+ # puts message
+#end
+
+def say_hello_world_five_times
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+  puts "Hello World!"
+end
+
+say_hello_world_five_times
+
+
+  
+
+
