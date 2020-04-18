6b699bb072c4e37b7a5c5c45ffd994ed2ec39de0
anna bateman <annacbateman@gmail.com>
2020-04-18 00:28:04 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,14 +1,20 @@
-def greeting
-  puts "Hello World"
+#def greeting
+ # puts "Hello World"
+#end
+
+#def greeting_five_times
+    greeting
+    greeting
+    greeting
+    greeting
+    greeting
+ # end
+  
+#greeting_five_times
+ 
+def greeting_a_person(name)
+  puts "Hello #{name}"
 end
 
-def greeting_five_times
-    greeting
-    greeting
-    greeting
-    greeting
-    greeting
-  end
-  
-greeting_five_times
-  
+
+
